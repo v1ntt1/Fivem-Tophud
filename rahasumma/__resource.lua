f9818cbca7f932f36bf82c6eb49ff07dfd984282
v1ntt1 <iirop2014@gmail.com>
2024-10-22 16:39:16 +0300

    Add files via upload

---
+++ rahasumma/__resource.lua
@@ -0,0 +1,12 @@
+
+shared_script '@es_extended/imports.lua'
+
+ui_page 'html/ui.html'
+
+client_script 'client.lua'
+
+files {
+	'html/ui.html',
+	'html/main.js',
+	'html/style.css',
+}
---
+++ server.lua
@@ -0,0 +1,51 @@
+ESX = exports["es_extended"]:getSharedObject()
+local RegisteredSocieties = {}
+
+TriggerEvent('es:addCommand', 'tili', function(source)
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	if xPlayer.job.grade_name == 'boss' then
+		local society = GetSociety(xPlayer.job.name)
+
+		if society ~= nil then
+			TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function(account)
+				money = account.money
+			end)
+		else
+			money = 0
+		end
+		
+		TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+		TriggerClientEvent("pNotify:SendNotification", source, {
+			text = "Firmasi tilillä on ".. money .."€!",
+			type = "success",
+			progressBar = false,
+			queue = "lmao",
+			timeout = 4000,
+			layout = "centerRight"
+		})
+	else
+		TriggerClientEvent("pNotify:SetQueueMax", source, "lmao", 5)
+		TriggerClientEvent("pNotify:SendNotification", source, {
+			text = "Oletko edes firman johdossa?",
+			type = "success",
+			progressBar = false,
+			queue = "lmao",
+			timeout = 4000,
+			layout = "centerRight"
+		})
+	end
+end, {help = "firman tilin"})
+
+TriggerEvent('esx_society:getSocieties', function(societies) 
+	RegisteredSocieties = societies
+end)
+
+function GetSociety(name)
+  for i=1, #RegisteredSocieties, 1 do
+    if RegisteredSocieties[i].name == name then
+      return RegisteredSocieties[i]
+    end
+  end
+end
